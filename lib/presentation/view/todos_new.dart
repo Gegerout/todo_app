392a9d20ca81e39ebe387e05cc18a1c712f4276b
Gegerout <selimchik06@gmail.com>
2023-04-16 21:42:53 +0300

    presentation update

--- lib/presentation/view/todos_new.dart
+++ lib/presentation/view/todos_new.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:shortid/shortid.dart';
 import 'package:todo_app/domain/models/todo.dart';
 import 'package:todo_app/domain/usecases/module.dart';
@@ -11,7 +12,7 @@ class TodosNew extends ConsumerStatefulWidget {
   ConsumerState<TodosNew> createState() => _TodosNewState();
 }
 
-class _TodosNewState extends State<TodosNew> {
+class _TodosNewState extends ConsumerState<TodosNew> {
   final _formKey = GlobalKey<FormState>();
   final title = TextEditingController();
   final description = TextEditingController();
@@ -21,60 +22,76 @@ class _TodosNewState extends State<TodosNew> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Add Todo"),
-        ),
-        body: Scrollbar(
-          child: SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              autovalidateMode: AutovalidateMode.always,
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: title,
-                    decoration: const InputDecoration(labelText: "Title"),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter a title";
+      appBar: AppBar(
+        title: const Text("Add Todo"),
+      ),
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.always,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: title,
+                  decoration: const InputDecoration(labelText: "Title"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter a title";
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: description,
+                  decoration: const InputDecoration(labelText: "Description"),
+                ),
+                CheckboxListTile(
+                    title: const Text("Completed"),
+                    value: isCompleted,
+                    onChanged: (value) {
+                      if (mounted) {
+                        setState(() {
+                          isCompleted = value!;
+                        });
                       }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: description,
-                    decoration: const InputDecoration(labelText: "Description"),
-                  ),
-                  CheckboxListTile(
-                      title: const Text("Completed"),
-                      value: isCompleted,
-                      onChanged: (value) {
-                        if(mounted) {
-                          setState(() {
-                            isCompleted = value!;
-                          });
-                        }
-                      })
-                ],
-              ),
+                    })
+              ],
             ),
           ),
         ),
+      ),
       floatingActionButton: FloatingActionButton.extended(
-          onPressed: () async {
-            if(_formKey.currentState!.validate()) {
-              _formKey.currentState!.save();
+        onPressed: () async {
+          if (_formKey.currentState!.validate()) {
+            _formKey.currentState!.save();
 
-              final todo = Todo(id: shortid.generate(), title: title.text, description: description.text, completed: isCompleted);
-              
-              final messenger = ScaffoldMessenger.of(context);
+            final todo = Todo(
+                id: shortid.generate(),
+                title: title.text,
+                description: description.text,
+                completed: isCompleted);
 
-              saveTodo(todo);
-            }
-          },
-          label: const Text('Save'),
+            final messenger = ScaffoldMessenger.of(context);
+            final router = GoRouter.of(context);
+
+            await saveTodo.execute(todo);
+            messenger.toast("Todo saved");
+            if(router.canPop()) router.pop();
+          }
+        },
+        label: const Text('Save'),
         icon: const Icon(Icons.save),
       ),
     );
+  }
+}
+
+extension on ScaffoldMessengerState {
+  void toast(String message) {
+    showSnackBar(SnackBar(
+      content: Text(message),
+      duration: const Duration(seconds: 2),
+    ));
   }
 }
--- lib/presentation/viewmodel/module.g.dart
+++ lib/presentation/viewmodel/module.g.dart
@@ -6,18 +6,18 @@ part of 'module.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$getTodosHash() => r'a579d9b254bc908fad7a7cedb81475459367f1c1';
+String _$todosListHash() => r'a311984d4a839307c7f8c70e6f4c540bd4902f76';
 
-/// See also [getTodos].
-@ProviderFor(getTodos)
-final getTodosProvider = AutoDisposeFutureProvider<dynamic>.internal(
-  getTodos,
-  name: r'getTodosProvider',
+/// See also [todosList].
+@ProviderFor(todosList)
+final todosListProvider = AutoDisposeFutureProvider<Todos>.internal(
+  todosList,
+  name: r'todosListProvider',
   debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$getTodosHash,
+      const bool.fromEnvironment('dart.vm.product') ? null : _$todosListHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef GetTodosRef = AutoDisposeFutureProviderRef<dynamic>;
+typedef TodosListRef = AutoDisposeFutureProviderRef<Todos>;
 // ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
